469c8e3ac6049e22f99f93c0482bb1270850629c
unknown <soppongaddo@gmail.com>
2023-03-06 15:12:27 +0000

    connect module to flask backend

--- final_project/server.py
+++ final_project/server.py
@@ -1,3 +1,7 @@
+import os
+import sys
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append(BASE_DIR)
 from machinetranslation import translator
 from flask import Flask, render_template, request
 import json
@@ -7,18 +11,20 @@ app = Flask("Web Translator")
 @app.route("/englishToFrench")
 def englishToFrench():
     textToTranslate = request.args.get('textToTranslate')
-    # Write your code here
-    return "Translated text to French"
+    translated_text = translator.english_to_french(textToTranslate)
+    translated_text = translated_text['translations'][0]['translation']
+    return translated_text
 
 @app.route("/frenchToEnglish")
 def frenchToEnglish():
     textToTranslate = request.args.get('textToTranslate')
-    # Write your code here
-    return "Translated text to English"
+    translated_text = translator.french_to_english(textToTranslate)
+    translated_text = translated_text['translations'][0]['translation']
+    return translated_text
 
 @app.route("/")
 def renderIndexPage():
-    # Write the code to render template
+    return render_template('index.html')
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
