e8bce55c1436b9b554879472836d8984817052df
unknown <soppongaddo@gmail.com>
2023-03-06 13:59:54 +0000

    added linting

--- final_project/machinetranslation/tests/test.py
+++ final_project/machinetranslation/tests/test.py
@@ -1,46 +1,66 @@
 import os
 import sys
 import unittest
-
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 sys.path.append(BASE_DIR)
-
 import translator
-
 class FrenchEnglishTestCase(unittest.TestCase):
-    
+    '''Class to test for french to english translator methods in translator module'''
     def test_null_input(self):
+        '''Function to test the french to english translation service 
+        and handle null entries in requests..
+        test name: test_null_input
+        Arguments: None'''
         input_text = ''
-        error_message = translator.frenchToEnglish(input_text)
-        self.assertEqual(error_message,
-            None
-        )
-        
-    
-    def test_hello_to_french(self):
-        input_text = 'Hello'
-        res = translator.englishToFrench(input_text)
-        output_text = res["translations"][0]["translation"]
-        self.assertEqual(output_text,"Bonjour")
-    
-
-class EnglishFrenchTestCase(unittest.TestCase):
-    
-    def test_is_null_input(self):
-        input_text = ''
-        error_message = translator.englishToFrench(input_text)
-        self.assertEqual(error_message,    
-            None
-        )
-
-
-    def test_bonjour_to_english(self):
+        error_message = translator.french_to_english(input_text)
+        self.assertEqual(error_message, None)
+    def test_french_to_english(self):
+        '''Function to test the french to english translation service,
+        converts french 'bonjour' to english 'Hello'
+        test name: test_french_to_english
+        Arguments: None
+        Returns:
+        True assert statement'''
         input_text = 'Bonjour'
-        res = translator.frenchToEnglish(input_text)
+        res = translator.french_to_english(input_text)
         output_text = res["translations"][0]["translation"]
         self.assertEqual(output_text,"Hello")
-        
-        
-
+    def test_not_bonjour_to_english(self):
+        '''Function to test false assert statements for the french to english s
+        ervice/function
+        Arguments: None
+        Returns:
+        Not Equal assert statement'''
+        input_text = 'Bonjour'
+        res = translator.french_to_english(input_text)
+        output_text = res["translations"][0]["translation"]
+        self.assertNotEqual(output_text,"My Bag")   
+class EnglishFrenchTestCase(unittest.TestCase):
+    '''Class to test for english to french translator methods in translator module'''
+    def test_is_null_input(self):
+        '''Function to test the french to english translation service,
+        converts french 'bonjour' to english 'Hello'
+        test name: test_french_to_english
+        Arguments: None
+        Returns:
+        True assert statement'''
+        input_text = ''
+        error_message = translator.english_to_french(input_text)
+        self.assertEqual(error_message, None)
+    def test_not_hello_to_french(self):
+        '''Function to test for incorrect french translations'''
+        input_text = 'Hello'
+        res = translator.english_to_french(input_text)
+        output_text = res["translations"][0]["translation"]
+        self.assertNotEqual(output_text,"S'appelle")         
+    def test_english_to_french(self):
+        '''Function to test english to french
+        test name: test_english_to_french
+        Returns:
+        Assert true statements'''
+        input_text = 'Hello'
+        res = translator.english_to_french(input_text)
+        output_text = res["translations"][0]["translation"]
+        self.assertEqual(output_text,"Bonjour")
 if __name__ == '__main__':
     unittest.main()
--- final_project/machinetranslation/translator.py
+++ final_project/machinetranslation/translator.py
@@ -1,59 +1,49 @@
-import json
-from ibm_watson import LanguageTranslatorV3, ApiException
-from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
 import os
-import sys
+from ibm_watson import LanguageTranslatorV3
+from ibm_watson import ApiException
+from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
 from dotenv import load_dotenv
-
 load_dotenv()
-
 apikey  = os.environ['APIKEY']
 url = os.environ['URL']
-
-
 translator_auth = IAMAuthenticator(apikey)
-language_translator = LanguageTranslatorV3(
-    version= '2018-05-01', 
-    authenticator=translator_auth
-    )
-
+language_translator = LanguageTranslatorV3(version= '2018-05-01', authenticator=translator_auth)
 language_translator.set_service_url(url)
-# language_translator.set_detailed_response(True)
-
-
-# create functions to translate from eng-frn and vice-versa
-
-def englishToFrench(englishText):
+def english_to_french(text):
+    '''
+    Function to convert english text to french 
+    -----------
+    Argument:
+    text:string
+    ------------
+    Returns
+    english text
+    ------------
+    '''
     try:
-        french_translation = language_translator.translate(
-        text = englishText,
+        french_translation = language_translator.translate(text = text,
         source = 'en',
         target = 'fr'
         ).get_result()
         return french_translation
-    except ApiException as error:
-        if str(error.message) == "400":
-            return "text is empty"
-        else:
-            error.message
-    
-
-
-def frenchToEnglish(frenchText):
+    except ApiException:
+        return None
+def french_to_english(text):
+    '''
+    Function to convert french text to english
+    -----------
+    Argument:
+    text:string
+    ------------
+    Returns
+    english text
+    ------------
+    '''
     try:
-        english_translation = language_translator.translate(
-            text = frenchText,
-            source = 'fr',
-            target = 'en'
-            ).get_result()
+        english_translation = language_translator.translate(text = text,
+        source = 'fr',
+        target = 'en'
+        ).get_result()
         return english_translation   
-    except ApiException as error:
-        if str(error.message) == "400":
-            return "text is empty"
-        else:
-            error.message
-
-
-
-
-
+    except ApiException:
+        return None
